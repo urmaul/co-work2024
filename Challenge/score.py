01879e2bdf031786955777c17c53b383bbab7585
Bogdan Kolbik <bogdan.kolbik@moia.io>
2024-09-26 15:05:25 +0200

    no nested quotes

---
+++ Challenge/score.py
@@ -0,0 +1,12 @@
+from solution import CourierRoute, InstanceSolution
+from read_data import Instance
+
+class Score:
+    def __init__(self, hard: int, soft: int):
+        self.hard = hard
+        self.soft = soft
+
+    def better_than(self, that: Score) -> bool:
+        return self.hard < that.hard or (self.hard == that.hard and self.soft < that.soft)
+
+# def solution_score(solution: InstanceSolution)
